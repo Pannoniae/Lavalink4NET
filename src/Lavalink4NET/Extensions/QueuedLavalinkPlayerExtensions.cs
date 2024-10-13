fc7e92658d8df49cebd0a4309b207010fa5bd227
NycroV <83246959+NycroV@users.noreply.github.com>
2024-10-12 23:50:02 -0400

    Fix "Adding a playlist via 'QueuedLavalinkPlayerExtensions' does not work" (#188)
    * Remove null propagation from queued extensions
    
    * Add documentation

--- src/Lavalink4NET/Extensions/QueuedLavalinkPlayerExtensions.cs
+++ src/Lavalink4NET/Extensions/QueuedLavalinkPlayerExtensions.cs
@@ -8,16 +8,25 @@ using Lavalink4NET.Rest.Entities.Tracks;
 
 public static class QueuedLavalinkPlayerExtensions
 {
+    /// <summary>
+    /// Adds a <see cref="TrackLoadResult"/> to the player's queue.<br/>
+    /// Returns the index of the track in the queue (or the last track added to the queue if <paramref name="loadResult"/> is a playlist)
+    /// </summary>
+    /// <param name="player">The player to enqueue the load result to.</param>
+    /// <param name="loadResult">The load result you want to enqueue.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>The index of the track in the queue (or the last track added to the queue if <paramref name="loadResult"/> is a playlist)</returns>
+    /// <exception cref="InvalidOperationException">When the <paramref name="loadResult"/> contains no tracks.</exception>
     public static async ValueTask<int> PlayAsync(this IQueuedLavalinkPlayer player, TrackLoadResult loadResult, CancellationToken cancellationToken = default)
     {
         cancellationToken.ThrowIfCancellationRequested();
 
-        var queueOffset = default(int?);
+        int? queueOffset = default;
 
         if (loadResult.Playlist is null)
         {
             // Single track
-            queueOffset ??= await player
+            queueOffset = await player
               .PlayAsync(loadResult.Track!, cancellationToken: cancellationToken)
               .ConfigureAwait(false);
         }
@@ -26,7 +35,7 @@ public static class QueuedLavalinkPlayerExtensions
             // Playlist
             foreach (var track in loadResult.Tracks)
             {
-                queueOffset ??= await player
+                queueOffset = await player
                   .PlayAsync(track, cancellationToken: cancellationToken)
                   .ConfigureAwait(false);
             }
